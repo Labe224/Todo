4eabf35151900b089f0ce9bedefe142f63954e0e
BARRY007 <alphabarryfr224@gmail.com>
2025-02-07 16:58:21 +0000

    v-responsive

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,10 +3,10 @@
         <div class="max-phone:bg-black max-phone:w-full max-phone:m-0 max-phone:p-2 max-phone:shadow-md">
             Daily_task
         </div>
-        <ul class="flex w-full gap-2.5 items-center max-phone:h-10 max-phone:justify-around max-phone:gap-0">
+        <ul class="flex w-full gap-2.5 items-center max-phone:h-10 justify-end max-phone:justify-around max-phone:gap-0">
             <li><input type="text" placeholder="Rechercher une tâche" class="w-80 h-8 text-white p-1 text-center border-b-1 boder-white max-phone:border-1 max-phone:rounded-full max-phone:border-black max-phone:text-black max-phone:" v-model="nom"></li>
             <button class=" p-1 bg-blue-500 shadow-xm shadow-zinc-100 hover:scale-110 hover:cursor-pointer focus:ring-1 focus:ring-white max-phone:hidden" @click="$emit('reach',nom)">Rechercher</button>
-            <i class="fas fa-search text-black bg-blue-600 p-2 phone:hidden" @click="$emit('reach',nom)"></i>
+           <div class="phone:hidden"> <i class="fas fa-search text-black bg-blue-600 p-2 " @click="$emit('reach',nom)"></i></div>
         </ul>
         
     </header>
--- src/components/cpnts_scndre/formulaire.vue
+++ src/components/cpnts_scndre/formulaire.vue
@@ -1,10 +1,10 @@
 <template>
 <form action="">
 
-    <div class="w-200 bg-zinc-400 h-110 m-auto mt-2" >
+    <div class="w-200 max-tab:w-full bg-zinc-400 h-110 m-auto mt-2" >
       <h1 class="text-center font-bold font-serif space-x-0.5 pt-2 text-2xl">Ajout d'une nouvelle tâche </h1>
       <div class="div"><span class="label">Tâche * :</span> <input type="text" required class="input" v-model="taches.nom"></div>
-      <div class="div"><span class="label">Durée * :</span> <div><input type="number" required class="time" v-model="heure"> Heures: <input type="number" class="time" v-model="min">Minutes </div></div>
+      <div class="div"><span class="label">Durée * :</span> <div><input type="number" required class="time" v-model="heure"> <span class="label">Heures:</span> <input type="number" class="time" v-model="min"> <span class="label">Minutes</span> </div></div>
       <div class="div"><span class="label">Date de debut * :</span> <input type="date" required class="input" v-model="taches.debut"></div>
       <div class="div"> <span class="label">Catégorie * :</span><select name="" id="select" class="input" v-model="taches.categorie">
         <option value="Moins Important">Moins Important</option>
@@ -12,11 +12,12 @@
         <option value="Très Important">Très important</option>
         <option value="Vitale">Vitale</option>
       </select></div>
-      <div class="div"> <span>Description:</span> <textarea v-model="taches.description" name="" id="" cols="80" rows="2" class="bg-zinc-700 text-zinc-300 text-center italic"> Entrez une description</textarea></div>
-  
-  <button class="bg-blue-200 text-white text-center p-1 w-35
-  hover:cursor-pointer shadow-md mt-4 ml-82 " @click.prevent="ajout"> Ajouter  </button>
+      <div class="div"> <span class="label">Description:</span> <textarea v-model="taches.description" name="" id="" cols="80" rows="2" class="bg-zinc-700 text-zinc-300 text-center italic max-tab:h-auto max-tab:mr-3"> Entrez une description</textarea></div>
+      <div class="w-1/2 m-auto flex justify-center"><button class="bg-blue-200 text-white text-center p-1 
+        hover:cursor-pointer shadow-md mt-4" @click.prevent="ajout"> Ajouter  </button> </div>
+
   </div>
+ 
   </form>
 </template>
 
